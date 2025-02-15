4f807ea1295204c0fd9fc01e177a63aeff950b2d
Opalami <sersim11@yandex.ru>
2025-02-15 21:55:54 +0300

    Init commit

---
+++ Calculator/main.swift
@@ -0,0 +1,92 @@
+//
+//  main.swift
+//  Calculator
+//
+//  Created by Максим Бондарев on 01.02.2025.
+//
+
+import Foundation
+import Darwin
+
+print("Welcome to CALCULATOR")
+
+var history: [String] = []
+
+while true{
+    
+    let operation = getDataFromUser(description: "Enter an operator: +, -, * or / (enter q to quit, h to show history): ")
+    
+    if operation == "q" {
+        exit(0)
+    }
+    
+    if operation == "h" {
+        showHistory()
+        continue
+        
+    }
+    
+    let firstNumber = getDataFromUser(description: "Enter the first number: ")
+    
+    let secondNumber = getDataFromUser(description: "Enter the second number: ")
+    
+    print("Counting " + firstNumber + " " + operation + " " + secondNumber + "...")
+    
+    history.append(firstNumber + " " + operation + " " + secondNumber)
+    
+    if let firstNumber = Double(firstNumber)
+    {
+        if let secondNumber = Double(secondNumber){
+            calculate(firstNumber, operation, secondNumber)
+        }
+        else {
+            print ("Invalid second number input")
+        }
+    }
+    
+    else {
+        print ("Invalid first number input")
+    }
+    print("\n\n\n")
+}
+
+func getDataFromUser(description disc: String) -> String {
+    print(disc)
+    return readLine() ?? ""
+}
+
+func showResult(_ result: Double){
+    print("Result is:   " + String(result))
+    history[history.count-1] = history[history.count-1] + " = " + String(result)
+}
+
+func calculate(_ firstNumber: Double, _ operation: String, _ secondNumber: Double){
+    switch operation {
+    case "+":
+        showResult(firstNumber + secondNumber)
+    case "-":
+        showResult(firstNumber - secondNumber)
+    case "*":
+        showResult(firstNumber * secondNumber)
+    case "/":
+        if secondNumber == 0 {
+            print("Error! Division by zero is not allowed.")
+            break
+        }
+        showResult(firstNumber - secondNumber)
+    default:
+        print("Invalid operator!")}
+    
+    
+}
+
+func showHistory(){
+    if history.isEmpty {
+        print("History is empty.")
+        return
+    }
+    print("History:")
+    for item in history {
+        print(item)
+    }
+}
